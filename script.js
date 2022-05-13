c91f6d85ae493d8b9437e72f576e01987ccad639
Valdinei de Paula Junior <valdineidepaulajunior@gmail.com>
2022-05-13 15:38:41 -0300

    bug fix in borders

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
+// const panels recebe todos os elementos com classe panel.
 const panels = document.querySelectorAll('.panel')
 
 panels.forEach(panel => {
+  // adciona a escuta de click em todos elementos de panels
   panel.addEventListener('click', () => {
-    removActiveClasses()
+    // chama a função removeActiveClases
+    removeActiveClasses()
+    // adciona a classe active ao elemento clicado
     panel.classList.add('active')
   })
 })
 
-function removActiveClasses() {
+// remove a classe active de todos elementos da const panels
+function removeActiveClasses() {
   panels.forEach(panel => {
     panel.classList.remove('active')
   })
